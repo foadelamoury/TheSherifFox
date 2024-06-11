b1d1fc3b17e514cdb98c3f0ffb0dba3743547552
MMarios <mariosmitoudis@gmail.com>
2024-06-11 22:17:27 +0300

    debug inventory

--- Scenes/inv_slot.gd
+++ Scenes/inv_slot.gd
@@ -1,9 +1,11 @@
 extends TextureRect
 
 @export var accessible = true
+@export var empty = true
 func  _ready():
 	if not accessible:
 		texture = null
+		empty = true
 	
 func _get_drag_data(at_position):
 	var preview_texture = TextureRect.new()
@@ -13,11 +15,13 @@ func _get_drag_data(at_position):
 	var preview = Control.new()
 	preview.add_child(preview_texture)
 	set_drag_preview(preview)
-	texture = null
 	accessible = false
-	return preview_texture.texture 
+	return self 
 func _can_drop_data(at_position, data):
-	return data is Texture2D
+	if data is TextureRect and not accessible:
+		return true
+	return false
 func _drop_data(at_position, data):
-	texture = data
+	texture = data.texture
+	data.texture = null
 	accessible = true
