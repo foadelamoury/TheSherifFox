fec6c211e45b2e683f9b7e89bff1420874c3143a
Fabian2611 <fabian.schwarz2010@gmail.com>
2024-06-13 17:08:27 +0200

    Room Generation almost working

--- Scripts/RoomGenerator.gd
+++ Scripts/RoomGenerator.gd
@@ -29,9 +29,9 @@ func render_dungeon(grid: Array[Array]):
 
 func make_grid(sizeX: int, sizeY: int) -> Array[Array]:
 	var grid: Array[Array] = []
-	for i in range(sizeX):
+	for i in range(sizeY):  # Correct loop limits
 		var row: Array = []
-		for j in range(sizeY):
+		for j in range(sizeX):
 			row.append(Room.new(Room.room_type.EMPTY, []))
 		grid.append(row)
 	return grid
@@ -40,9 +40,9 @@ func generate_dungeon(grid: Array[Array], branch_length: int, dead_end_chance: f
 	var _grid: Array[Array] = grid
 	var x_size: int = _grid[0].size()
 	var y_size: int = _grid.size()
-
-	var last_coordinate: Array[int] = [0, 0]
-	var current_coordinate: Array[int] = [floor(float(y_size) / 2.0), 0]
+	
+	var last_coordinate: Array = [0, 0]
+	var current_coordinate: Array = [floor(float(y_size) / 2.0), 0]
 	var current_x: int = current_coordinate[1]
 	var current_y: int = current_coordinate[0]
 
@@ -50,36 +50,8 @@ func generate_dungeon(grid: Array[Array], branch_length: int, dead_end_chance: f
 	_grid[current_y][current_x] = Room.new(Room.room_type.START, [])
 
 	for i in range(branch_length):
-		# Get neighbours
-		var north: Array[int] = [0, 0]
-		var east: Array[int] = [0, 0]
-		var south: Array[int] = [0, 0]
-		var west: Array[int] = [0, 0]
+		var directions: Array[Array] = get_directions(x_size, y_size, current_x, current_y, _grid)
 
-		var dir_states: Array[Room.direction] = []
-
-		# Only set if neighbour is valid
-		if current_y != 0:
-			north = [current_y - 1, current_x]
-			dir_states.append(Room.direction.NORTH)
-		if current_y != y_size - 1:
-			south = [current_y + 1, current_x]
-			dir_states.append(Room.direction.SOUTH)
-		if current_x != x_size - 1:
-			east = [current_y, current_x + 1]
-			dir_states.append(Room.direction.EAST)
-		if current_x != 0:
-			west = [current_y, current_x - 1]
-			dir_states.append(Room.direction.WEST)
-
-		var directions: Array = []
-
-		# Check valid directions
-		for dir in [north, south, east, west]:
-			if dir != [0, 0] and _grid[dir[0]][dir[1]].type == Room.room_type.EMPTY:
-				directions.append(dir)
-
-		# Set room
 		if directions.size() > 0:
 			last_coordinate = current_coordinate
 			current_coordinate = directions[randi_range(0, directions.size() - 1)]
@@ -108,21 +80,83 @@ func generate_dungeon(grid: Array[Array], branch_length: int, dead_end_chance: f
 			print("GEN001 | (" + str(current_x) + ", " + str(current_y) + ") with direction " + str(inverse_room_dir) + " invoked (" + str(last_coordinate[1]) + ", " + str(last_coordinate[0]) + ") to have direction " + str(room_dir).replace("0", "N").replace("1", "E").replace("2", "S").replace("3", "W"))
 
 			_grid[last_coordinate[0]][last_coordinate[1]].directions.append(room_dir)
-			_grid[current_y][current_x] = Room.new(Room.room_type.END, []) if i == branch_length - 1 else Room.new(Room.room_type.GENERIC, [inverse_room_dir])
+			_grid[current_y][current_x] = Room.new(Room.room_type.END, []) if i == branch_length - 1 else Room.new(Room.room_type.MAIN_BRANCH, [inverse_room_dir])
 		else:
-			push_error("GEN401 | Room has no valid directions.")
 			return -1
+			
+	print("GEN002 | Generating main branch finished. Starting extra paths.")
+	
+	var dead_end_count: int = 0
+	
+	current_y = 0
+	for row: Array[Room] in _grid:
+		current_x = 0
+		for room: Room in row:
+			if room.type == Room.room_type.MAIN_BRANCH or room.type == Room.room_type.START:
+				if randf() <= dead_end_chance and dead_end_count < dead_end_max:
+					dead_end_count += 1
+					var directions: Array[Array] = get_directions(x_size, y_size, current_x, current_y, _grid)
+					
+					if directions.size() == 0:
+						return -1
+					last_coordinate = [current_y, current_x]
+					current_coordinate = directions[randi_range(0, directions.size() - 1)]
+					current_x = current_coordinate[1]
+					current_y = current_coordinate[0]
+					
+					var dx = current_x - last_coordinate[1]
+					var dy = current_y - last_coordinate[0]
+
+					var room_dir: Room.direction
+					var inverse_room_dir: Room.direction
+
+					if dy < 0:
+						room_dir = Room.direction.NORTH
+						inverse_room_dir = Room.direction.SOUTH
+					elif dy > 0:
+						room_dir = Room.direction.SOUTH
+						inverse_room_dir = Room.direction.NORTH
+					elif dx < 0:
+						room_dir = Room.direction.WEST
+						inverse_room_dir = Room.direction.EAST
+					elif dx > 0:
+						room_dir = Room.direction.EAST
+						inverse_room_dir = Room.direction.WEST
+
+					print("GEN003 | (" + str(current_x) + ", " + str(current_y) + ") with direction " + str(inverse_room_dir) + " invoked (" + str(last_coordinate[1]) + ", " + str(last_coordinate[0]) + ") to have direction " + str(room_dir).replace("0", "N").replace("1", "E").replace("2", "S").replace("3", "W"))
+
+					_grid[last_coordinate[0]][last_coordinate[1]].directions.append(room_dir)
+					_grid[current_y][current_x] = Room.new(Room.room_type.GENERIC, [inverse_room_dir])
+			current_x += 1
+		current_y += 1
 
 	return _grid
+
+func get_directions(max_x: int, max_y: int, x: int, y: int, grid: Array[Array]) -> Array[Array]:
+	var directions: Array[Array] = []
+
+	# Only set if neighbour is valid
+	if y > 0 and grid[y - 1][x].type == Room.room_type.EMPTY:
+		directions.append([y - 1, x])
+	if y < max_y - 1 and grid[y + 1][x].type == Room.room_type.EMPTY:
+		directions.append([y + 1, x])
+	if x > 0 and grid[y][x - 1].type == Room.room_type.EMPTY:
+		directions.append([y, x - 1])
+	if x < max_x - 1 and grid[y][x + 1].type == Room.room_type.EMPTY:
+		directions.append([y, x + 1])
+
+	return directions
 
 func _ready():
 	var generation_done: bool = false
 	var grid: Variant
-	while !generation_done:
+	while not generation_done:
 		grid = make_grid(x, y)
-		grid = generate_dungeon(grid, 5, 0.0, 0)
-		if grid:
+		grid = generate_dungeon(grid, 5, 0.75, 3)
+		if grid is Array[Array]:
 			generation_done = true
+	print(grid)
+	render_dungeon(grid)
 
 func _process(delta):
 	pass
